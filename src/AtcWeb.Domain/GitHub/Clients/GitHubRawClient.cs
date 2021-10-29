0d911b752e7fc3d5b555bb8f9a95ffdf3e72eb4a
David Kallesen <david@lmdk.dk>
2021-10-29 10:35:32 +0200

    Remove single-line... to fix build

--- src/AtcWeb.Domain/GitHub/Clients/GitHubRawClient.cs
+++ src/AtcWeb.Domain/GitHub/Clients/GitHubRawClient.cs
@@ -21,7 +21,6 @@ namespace AtcWeb.Domain.GitHub.Clients
         public async Task<(bool isSuccessful, string)> GetRawAtcCodeFile(string repositoryName, string defaultBranchName, string filePath, CancellationToken cancellationToken)
         {
             // TODO: Add Locks
-
             var url = $"/atc-net/{repositoryName}/{defaultBranchName}/{filePath}";
             var cacheKey = $"{CacheConstants.CacheKeyCodeFile}_{url}";
             if (memoryCache.TryGetValue(cacheKey, out string data))
