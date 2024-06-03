a83f6ee0988ad234244dba5325919f284b88582d
Per Kops <pko@delegate.dk>
2024-06-03 14:03:26 +0200

    feat: added Atc.Microsoft.Graph.Client repository

---
+++ src/AtcWeb/Pages/Repository/AtcMicrosoftGraphClient.razor.cs
@@ -0,0 +1,9 @@
+namespace AtcWeb.Pages.Repository;
+
+public class AtcMicrosoftGraphClientBase : RepositoryComponentBase
+{
+    public AtcMicrosoftGraphClientBase()
+        : base("atc-microsoft-graph-client")
+    {
+    }
+}
