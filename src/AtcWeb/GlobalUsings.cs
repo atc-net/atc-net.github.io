1f3b8b69769d377d17bf6ab28318b6477df79d72
Per Kops <pko@delegate.dk>
2026-03-30 23:51:49 +0200

    feat(news): redesign as timeline with search and skeleton loading
    - Replace boring table with MudTimeline and accent cards
    - Color-coded chips per news action type
    - Search filter + year dropdown (removed slow repo dropdown)
    - Skeleton loading cards instead of spinner
    - Cap display at 50 items for performance

--- src/AtcWeb/GlobalUsings.cs
+++ src/AtcWeb/GlobalUsings.cs
@@ -1,8 +1,8 @@
 global using System.Diagnostics.CodeAnalysis;
 global using System.Text.RegularExpressions;
 global using System.Web;
-
 global using Atc;
+global using AtcWeb;
 global using AtcWeb.Domain.AtcApi;
 global using AtcWeb.Domain.AtcApi.Models;
 global using AtcWeb.Domain.Data.Models;
@@ -12,16 +12,11 @@ global using AtcWeb.Extensions;
 global using AtcWeb.Models;
 global using AtcWeb.State;
 global using AtcWeb.Styles;
-
 global using ColorCode.Styling;
-
 global using Ganss.Xss;
-
 global using Markdig;
-
 global using Microsoft.AspNetCore.Components;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 global using Microsoft.AspNetCore.Components.WebAssembly.Http;
-
 global using MudBlazor;
 global using MudBlazor.Services;
