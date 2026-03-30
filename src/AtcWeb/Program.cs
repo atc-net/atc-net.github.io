13758136c89a758b3d8f7797dbe83ab6a09a7b60
Per Kops <pko@delegate.dk>
2026-03-30 23:51:15 +0200

    refactor: convert Program.cs to top-level statements

--- src/AtcWeb/Program.cs
+++ src/AtcWeb/Program.cs
@@ -1,35 +1,28 @@
-namespace AtcWeb;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-public static class Program
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(_ => new DefaultBrowserOptionsMessageHandler
 {
-    public static Task Main(string[] args)
-    {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
+    DefaultBrowserRequestCache = BrowserRequestCache.NoStore,
+    DefaultBrowserRequestMode = BrowserRequestMode.NoCors,
+});
 
-        builder.Services.AddScoped(_ => new DefaultBrowserOptionsMessageHandler
-        {
-            DefaultBrowserRequestCache = BrowserRequestCache.NoStore,
-            DefaultBrowserRequestMode = BrowserRequestMode.NoCors,
-        });
+builder.Services.AddScoped<AtcApiGitHubApiInformationClient>();
+builder.Services.AddScoped<AtcApiGitHubRepositoryClient>();
+builder.Services.AddScoped<GitHubRepositoryService>();
+builder.Services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(_ =>
+{
+    var sanitizer = new HtmlSanitizer();
+    sanitizer.AllowedAttributes.Add("class");
+    sanitizer.AllowedAttributes.Add("id");
+    return sanitizer;
+});
 
-        builder.Services.AddScoped<AtcApiGitHubApiInformationClient>();
-        builder.Services.AddScoped<AtcApiGitHubRepositoryClient>();
-        builder.Services.AddScoped<GitHubRepositoryService>();
-        builder.Services.AddScoped<IHtmlSanitizer, HtmlSanitizer>(_ =>
-        {
-            var sanitizer = new HtmlSanitizer();
-            sanitizer.AllowedAttributes.Add("class");
-            sanitizer.AllowedAttributes.Add("id");
-            return sanitizer;
-        });
+builder.Services.AddSingleton<StateContainer>();
 
-        builder.Services.AddSingleton<StateContainer>();
+builder.Services.AddMemoryCache();
 
-        builder.Services.AddMemoryCache();
+builder.Services.AddMudServices();
 
-        builder.Services.AddMudServices();
-
-        return builder.Build().RunAsync();
-    }
-}
+await builder.Build().RunAsync();
