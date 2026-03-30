c327e69201374ed68f2be9b151d31b11aea4113d
davidkallesen <david@lmdk.dk>
2026-03-30 14:36:27 +0200

    style(theme): add dark mode palette colors

--- src/AtcWeb/Styles/MudThemeHelper.cs
+++ src/AtcWeb/Styles/MudThemeHelper.cs
@@ -11,7 +11,36 @@ public static class MudThemeHelper
 
     public static readonly PaletteDark DarkPalette = new()
     {
-        Primary = "#6EAEDF",
+        Primary = "#776be7",
+        Secondary = "#ff4081",
+        Tertiary = "#1ec8a5",
+        Info = "#3299ff",
+        Success = "#0bba83",
+        Warning = "#ffa800",
+        Error = "#f64e62",
+        Black = "#27272f",
+        Background = "#1a1a27",
+        BackgroundGray = "#151521",
+        Surface = "#1e1e2d",
+        DrawerBackground = "#1a1a27",
+        DrawerText = "#92929f",
+        DrawerIcon = "#92929f",
+        AppbarBackground = "#262638",
+        AppbarText = "#c8c6d6",
+        TextPrimary = "#b2b0bf",
+        TextSecondary = "#92929f",
+        TextDisabled = "rgba(255,255,255,0.2)",
+        ActionDefault = "#74718e",
+        ActionDisabled = "rgba(153,153,153,0.3)",
+        ActionDisabledBackground = "rgba(96,95,109,0.3)",
+        Divider = "#292838",
+        DividerLight = "rgba(255,255,255,0.06)",
+        TableLines = "#33323e",
+        LinesDefault = "#33323e",
+        LinesInputs = "rgba(255,255,255,0.3)",
+        GrayLight = "#2a2833",
+        GrayLighter = "#1e1e2d",
+        OverlayLight = "#1e1e2d80",
     };
 
     public static readonly LayoutProperties LayoutProperties = new()
