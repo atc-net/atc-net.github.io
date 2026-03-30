493456150ce2d82591e4eb06f3517d9cdc4f153d
davidkallesen <david@lmdk.dk>
2026-03-30 11:06:49 +0200

    chore: remove unused pages and data
    - Delete GitHubContributorGrid component
    - Delete GeneralRulesAndGuidelines page
    - Remove unused responsible members data from RepositoryMetadata

--- src/AtcWeb.Domain/Data/RepositoryMetadata.cs
+++ src/AtcWeb.Domain/Data/RepositoryMetadata.cs
@@ -15,54 +15,6 @@ public static class RepositoryMetadata
         "netstandard2.1",
     ];
 
-    private static readonly string[] DefaultResponsibleMembers = ["davidkallesen", "perkops"];
-
-    /// <summary>
-    /// Overrides for repositories that have different responsible members than the defaults.
-    /// Repositories not listed here will use <see cref="DefaultResponsibleMembers"/>.
-    /// </summary>
-    private static readonly Dictionary<string, string[]> ResponsibleMemberOverrides = new(StringComparer.Ordinal)
-    {
-        ["atc-autoformatter"] = ["rickykaare"],
-        ["atc-azure-digitaltwin"] = ["perkops"],
-        ["atc-azure-messaging"] = ["christianhelle"],
-        ["atc-azure-options"] = ["kimlundjohansen"],
-        ["atc-cosmos"] = ["rickykaare"],
-        ["atc-cosmos-eventstore"] = ["LarsSkovslund"],
-        ["atc-data-platform"] = ["davidkallesen", "perkops", "mrmasterplan"],
-        ["atc-data-platform-tools"] = ["davidkallesen", "perkops", "mrmasterplan"],
-        ["atc-react"] = ["perkops"],
-        ["atc-rest-client"] = ["davidkallesen", "perkops", "LarsSkovslund", "egil"],
-        ["atc-semantic-kernel"] = ["perkops"],
-        ["atc-snippets"] = ["perkops", "lupusbytes"],
-        ["atc-test"] = ["rickykaare"],
-        ["atc-wpf"] = ["davidkallesen"],
-    };
-
-    public static string[] GetResponsibleMembersByName(string name)
-    {
-        if (string.IsNullOrEmpty(name))
-        {
-            return ResponsibleMemberOverrides
-                .SelectMany(x => x.Value)
-                .Concat(DefaultResponsibleMembers)
-                .Distinct(StringComparer.Ordinal)
-                .OrderBy(x => x, StringComparer.Ordinal)
-                .ToArray();
-        }
-
-        var members = ResponsibleMemberOverrides.TryGetValue(name, out var overrides)
-            ? overrides
-            : DefaultResponsibleMembers;
-
-        return members
-            .OrderBy(x => x, StringComparer.Ordinal)
-            .ToArray();
-    }
-
-    public static bool HasNotRequiredResponsibleMembersByName(string name)
-        => GetResponsibleMembersByName(name).Length < 2;
-
     public static bool IsVisualStudioNameInAcceptedVersion(
         string visualStudioName)
         => !string.IsNullOrEmpty(visualStudioName) &&
