c9c38db8bd065aa6d7487635db588bac108b7bda
davidkallesen <david@lmdk.dk>
2026-03-30 11:25:23 +0200

    refactor(api): extract shared base URL to AtcApiConstants

---
+++ src/AtcWeb.Domain/AtcApi/AtcApiConstants.cs
@@ -0,0 +1,6 @@
+namespace AtcWeb.Domain.AtcApi;
+
+internal static class AtcApiConstants
+{
+    internal const string BaseAddress = "https://atcnet-api-prod-api-ca-01.greenhill-862ffb7c.swedencentral.azurecontainerapps.io";
+}
